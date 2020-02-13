e70f9f43fce96a890884387ad85721fbfd211470
Geo Pertea <geo.pertea@gmail.com>
2020-02-13 09:55:33 -0500

    repository archived; use https://github.com/gpertea/stringtie instead

--- SuperReads_RNA/global-1/ufasta/tests/test_split.sh
+++
@@ -1,5 +0,0 @@
-
-trap "rm -f split1 split2" EXIT
-ufasta split -i $TEST1 >(cat > split1) >(cat > split2)
-diff <(cat split1 split2 | ufasta sort) <(ufasta sort $TEST1)
-EXPECT_SUCCESS "Split join"
--- tag_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-ver=$(fgrep '#define VERSION ' stringtie.cpp)
-ver=${ver#*\"}
-ver=${ver%%\"*}
-git checkout master
-git tag -a "v$ver" -m "release $ver"
-git push --tags
